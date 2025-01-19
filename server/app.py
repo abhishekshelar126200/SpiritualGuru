8893068a4b09cb5b9304e3bf271f7a03aed2ed85
Abhishek Balkrishna Shelar <116205134+abhishekshelar126200@users.noreply.github.com>
2025-01-19 14:19:57 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -169,4 +169,4 @@ def chatbot():
         return jsonify({"msg": str(e), "error_type": type(e).__name__}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(host="0.0.0.0", port=5000)
